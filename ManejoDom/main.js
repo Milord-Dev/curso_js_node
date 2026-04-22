7799a4a6fd46dd26befcf5f872a827a44b99f45f
ricardo milord <milord.dev05@gmail.com>
2026-04-22 00:36:46 -0500

    feat: agregar id al formulario en index.html y limpiar código comentado en main.js

--- ManejoDom/main.js
+++ ManejoDom/main.js
@@ -1,6 +1,19 @@
+// Codigo antetior de prueba
+// window.onload = () => {
+//     //como podemos conseguir elementos o objetos del dom 
+//     const texto = document.getElementById('texto')
+//     texto.innerText = "To-do List" // con innerText cambia o agrega el texto
+//     // texto.innerHTML = '<li>Hola</li> <li>Mundo</li>' // con innerHTML agrega html al elemento
+// }
+
 window.onload = () => {
-    //como podemos conseguir elementos o objetos del dom 
-    const texto = document.getElementById('texto')
-    texto.innerText = "To-do List" // con innerText cambia o agrega el texto
-    // texto.innerHTML = '<li>Hola</li> <li>Mundo</li>' // con innerHTML agrega html al elemento
+    const form = document.getElementById('todo-form')
+    //el on submit es una propiedad de js
+    form.onsubmit = (e) => {
+        e.preventDefault();
+        const todo = document.getElementById('to-do')
+        todoValor = todo.value;
+        todo.value = '';
+        console.log(todoValor)
+    }
 }
